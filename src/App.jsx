ca3f2218ac7d674d25dc01f6a85d84f7d6151f4b
Enzo0708 <enzobiason7@gmail.com>
2023-11-21 19:13:04 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@
 import Exercicio1 from './components/exc1';
 import Exercicio2 from './components/exc2';
 import Header from './components/exc3';
-import ClienteComponent from './components/exc4';
-import Produtos from './components/exc5';
+import ClienteGastos from './components/exc4';
+import ProdutosMaisCaros from './components/exc5';
 
 function App(){
   return(
@@ -23,12 +23,11 @@ function App(){
       <Exercicio1 />
       <Exercicio2 />
       // exercício 4
-      <ClienteComponent {...luana} />
-      <ClienteComponent {...mario} />
+      <ClienteGastos />
       // exercício 5
       <div>
       <h2>Produtos Mais Caros que R$ 1500</h2>
-      <Produtos produtos={produtos} />
+      <ProdutosMaisCaros />
   </div>
     </>
   )
